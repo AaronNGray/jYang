64a4771d2b2c767ab40f68032391272771a77bef
nataf <nataf@f719c1b9-cf58-0410-9e4f-c9f8fe84afc8>
2010-06-02 12:13:47 +0000

    Branch for Dsdl generation

---
+++ branches/dsdl/jyang/parser/YANG_DeviateNotSupported.java
@@ -0,0 +1,21 @@
+
+package jyang.parser;
+
+
+public class YANG_DeviateNotSupported extends SimpleYangNode {
+  public YANG_DeviateNotSupported(int id) {
+    super(id);
+  }
+
+  public YANG_DeviateNotSupported(yang p, int id) {
+    super(p, id);
+  }
+  
+  public String toString() {
+	  String result = "";
+	  result += "deviate not-supported";
+	  result += ";";
+	  return result;
+  }
+
+}
---
+++ branches/dsdl/jyang/parser/YANG_ShortCase.java
@@ -0,0 +1,9 @@
+package jyang.parser;
+
+
+public interface YANG_ShortCase  extends YangNode{
+
+    public void check(YangContext context);
+    public String getBody();
+
+}
