5273c82b2376675713fd3e40e505aa913a8ae889
nataf <nataf@f719c1b9-cf58-0410-9e4f-c9f8fe84afc8>
2010-06-02 12:13:47 +0000

    Branch for Dsdl generation

--- www/jYang/toDo/Entrees/2009/6/22_xPath_files/22_xPath.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.1 local-build-20090624
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('22_xPath_files/22_xPathMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();BlogFixupPreviousNext();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- www/jYang/toDo/Entrees/2009/6/24_config_files/24_config.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.1 local-build-20090624
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('24_config_files/24_configMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();BlogFixupPreviousNext();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
